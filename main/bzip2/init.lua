4a4e91aa889dbdc59b3bee0aed21f9215fb68f5b
Stilic <lateman-jpeg@outlook.fr>
2025-06-29 20:18:24 +0200

    nano: fix libs

--- main/bzip2/init.lua
+++ main/bzip2/init.lua
@@ -15,15 +15,15 @@ function self.build()
     os.execute("make libbz2.a bzip2 bzip2recover" .. cflags .. system.get_make_jobs())
     os.execute("make" .. cflags .. "-f Makefile-libbz2_so" .. system.get_make_jobs())
     lfs.mkdir("_install")
-    os.execute('make install PREFIX="' .. lfs.currentdir() .. '/_install/usr"')
+    os.execute('make install PREFIX="' .. lfs.currentdir() .. '/_install"')
 end
 
 function self.pack()
     os.execute("cp -ra source/_install/* filesystem")
-    os.execute("cp source/libbz2.so." .. self.version .. " filesystem/usr/lib")
-    os.execute("ln -s libbz2.so." .. self.version .. " filesystem/usr/lib/libbz2.so")
-    os.execute("ln -s libbz2.so." .. self.version .. " filesystem/usr/lib/libbz2.so.1")
-    os.execute("ln -s libbz2.so." .. self.version .. " filesystem/usr/lib/libbz2.so.1.0")
+    os.execute("cp source/libbz2.so." .. self.version .. " filesystem/lib")
+    os.execute("ln -s libbz2.so." .. self.version .. " filesystem/lib/libbz2.so")
+    os.execute("ln -s libbz2.so." .. self.version .. " filesystem/lib/libbz2.so.1")
+    os.execute("ln -s libbz2.so." .. self.version .. " filesystem/lib/libbz2.so.1.0")
 end
 
 return self
