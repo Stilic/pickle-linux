bb0facd6050899a1030d35f9c1415639471df205
Stilic <lateman-jpeg@outlook.fr>
2025-07-08 13:22:41 +0200

    add turnstiled

---
+++ main/turnstile/init.lua
@@ -0,0 +1,22 @@
+local lfs = require "lfs"
+local tools = require "tools"
+
+local self = {}
+
+-- TODO: add scdoc
+self.version = "0.1.10"
+self.dev_dependencies = { pkg "user.muon", pkg "user.pkgconf" }
+self.sources = {
+    { "source", "https://github.com/chimera-linux/turnstile/archive/refs/tags/v" .. self.version .. ".tar.gz" }
+}
+
+self.build = tools.build_meson("/", "-Ddinit=enabled -Drunit=disabled -Dpam_moddir=/lib/security -Dmanage_rundir=true")
+function self.pack()
+    tools.pack_default()()
+
+    os.execute("cp ../turnstiled filesystem/etc/dinit.d")
+    lfs.mkdir("filesystem/etc/dinit.d/boot.d")
+    lfs.link("../turnstiled", "filesystem/etc/dinit.d/boot.d/turnstiled", true)
+end
+
+return self
