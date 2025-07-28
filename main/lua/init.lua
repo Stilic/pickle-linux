3b1af421fd9becb32cd70e80390445f2aae197d1
Stilic <lateman-jpeg@outlook.fr>
2025-07-28 13:38:05 +0200

    fix lua

--- main/lua/init.lua
+++ main/lua/init.lua
@@ -1,5 +1,6 @@
 local lfs = require "lfs"
 local system = require "system"
+local tools = require "tools"
 
 local self = {}
 
@@ -10,15 +11,20 @@ self.sources = {
 
 function self.build()
     lfs.chdir("source")
-    -- TODO: see if we can add cflags there
-    os.execute("make" .. system.get_make_jobs())
+
+    -- TODO: see if we can add cflags there + readline support
+    os.execute("make linux" .. system.get_make_jobs())
     os.execute("make local")
+
+    lfs.mkdir("install/lib/pkgconfig")
+    local pc = io.open("install/lib/pkgconfig/lua5.4.pc", "w")
+    pc:write(system.capture("make pc"))
+    pc:close()
 end
 
 function self.pack()
-    lfs.mkdir("filesystem/usr")
-    os.execute("cp -ra source/install/* filesystem/usr")
-    lfs.link("/usr/bin/lua", "filesystem/usr/bin/lua" ..
+    tools.pack_default("source/install")()
+    lfs.link("/bin/lua", "filesystem/bin/lua" ..
         self.version:sub(1, self.version:find(".", self.version:find(".", 1, true) + 1, true) - 1), true)
 end
 
