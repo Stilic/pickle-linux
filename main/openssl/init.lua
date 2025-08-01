9e1def87f2f0d2d341bfcebd7f68b2ce9b0104e9
Stilic <lateman-jpeg@outlook.fr>
2025-08-01 23:21:39 +0200

    move openssl to main

--- main/openssl/init.lua
+++ main/openssl/init.lua
@@ -7,10 +7,10 @@ self.sources = {
     { "source", "https://github.com/openssl/openssl/releases/download/openssl-" .. self.version .. "/openssl-" .. self.version .. ".tar.gz" }
 }
 
-self.build = tools.build_gnu_configure(nil, nil, nil, nil, nil, "Configure")
+self.build = tools.build_gnu_configure("", nil, nil, nil, nil, "Configure")
 function self.pack()
     tools.pack_default()()
-    os.rename("filesystem/usr/lib64", "filesystem/usr/lib")
+    os.rename("filesystem/lib64", "filesystem/lib")
 end
 
 return self
