44524d708c8d2f7e70adfdb8a51eccecb4977eed
Stilic <lateman-jpeg@outlook.fr>
2025-08-02 00:15:20 +0200

    really sorry

--- main/toybox/init.lua
+++ main/toybox/init.lua
@@ -16,10 +16,6 @@ end
 function self.pack()
     os.execute("cp -ra source/install/bin source/install/sbin filesystem")
     os.execute("cp -ra source/install/usr/bin source/install/usr/sbin filesystem")
-
-    lfs.mkdir("filesystem/usr")
-    lfs.mkdir("filesystem/usr/bin")
-    lfs.link("../../bin/env", "filesystem/usr/bin/env", true)
 end
 
 return self
