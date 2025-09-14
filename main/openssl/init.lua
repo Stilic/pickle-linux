f32d8ca125337d50bbc0e2835d2623dcba9be293
Stilic <lateman-jpeg@outlook.fr>
2025-09-14 15:40:11 +0200

    openssl: move config to /etc

--- main/openssl/init.lua
+++ main/openssl/init.lua
@@ -7,7 +7,7 @@ self.sources = {
     { "source", "https://github.com/openssl/openssl/releases/download/openssl-" .. self.version .. "/openssl-" .. self.version .. ".tar.gz" }
 }
 
-self.build = tools.build_gnu_configure("/", nil, nil, nil, nil, "Configure")
+self.build = tools.build_gnu_configure("/", "--openssldir=/etc/ssl", nil, nil, nil, "Configure")
 function self.pack()
     tools.pack_default()()
     os.rename("filesystem/lib64", "filesystem/lib")
