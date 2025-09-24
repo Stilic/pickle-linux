509eafb4af337fdaad9587bc4d589155109885b4
Stilic <lateman-jpeg@outlook.fr>
2025-09-24 23:03:54 +0200

    linux: REALLY fix headers i hate `make headers_install`

--- main/linux/init.lua
+++ main/linux/init.lua
@@ -26,8 +26,9 @@ function self.pack()
         "ZSTD_CLEVEL=19 make INSTALL_MOD_PATH=../filesystem INSTALL_MOD_STRIP=1 DEPMOD=/doesnt/exist modules_install")
     os.remove(path .. "/build")
 
-    os.execute("make mrproper headers_install")
-    os.execute("cp -ra include ../filesystem")
+    os.execute("make mrproper headers")
+    os.execute("cp -ra usr/include ../filesystem")
+    os.execute("find ../filesystem/include ! -name '*.h' -type f -delete")
 end
 
 return self
