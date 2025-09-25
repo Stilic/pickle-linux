37b1ae5843a5370a84240ccc9b9cb9c1e1764247
Stilic <lateman-jpeg@outlook.fr>
2025-09-25 21:30:22 +0200

    python: fix

--- user/python/init.lua
+++ user/python/init.lua
@@ -13,12 +13,14 @@ self.build = tools.build_gnu_configure(nil, "--enable-shared --with-system-expat
 function self.pack()
     tools.pack_default()()
 
-    for _, v in ipairs({ "python" .. self.version[1], "idle" .. self.version[1], "pydoc" .. self.version[1] }) do
+    local major_version = self.version:sub(1, 1)
+
+    for _, v in ipairs({ "python" .. major_version, "idle" .. major_version, "pydoc" .. major_version }) do
         lfs.link(v, "filesystem/usr/bin/" .. v:sub(1, -2), true)
     end
 
-    lfs.link("python" .. self.version[1] .. "-config", "filesystem/usr/bin/python-config", true)
-    lfs.link("python" .. self.version[1] .. ".1", "filesystem/usr/share/man/man1/python.1", true)
+    lfs.link("python" .. major_version .. "-config", "filesystem/usr/bin/python-config", true)
+    lfs.link("python" .. major_version .. ".1", "filesystem/usr/share/man/man1/python.1", true)
 end
 
 return self
