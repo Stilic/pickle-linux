63563253bc5e4029a732a84860854a1531ff1b2c
Stilic <lateman-jpeg@outlook.fr>
2025-09-25 23:50:55 +0200

    duh

--- user/python-installer/init.lua
+++ user/python-installer/init.lua
@@ -12,7 +12,7 @@ self.sources = {
 function self.pack()
     local install_dir = "filesystem/usr/lib/python" .. python.short_version .. "/site-packages"
     os.execute("mkdir -p " .. install_dir)
-    os.execute("cp -ra source/* " .. install_dir)
+    os.execute("cp -ra source/installer " .. install_dir)
     os.execute("python3 -m compileall " .. install_dir)
 end
 
