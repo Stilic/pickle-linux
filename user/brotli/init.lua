e994aa52664aa185cc6bda3525cc89ee2e0645a0
Stilic <lateman-jpeg@outlook.fr>
2025-09-25 20:36:34 +0200

    tools: add cmake builder

--- user/brotli/init.lua
+++ user/brotli/init.lua
@@ -1,4 +1,6 @@
 local lfs = require "lfs"
+local system = require "system"
+local tools  = require "tools"
 
 local self = {}
 
@@ -8,17 +10,9 @@ self.sources = {
     { "source", "https://github.com/google/brotli/archive/refs/tags/v" .. self.version .. ".tar.gz" }
 }
 
--- TODO: add cflags
-function self.build()
-    lfs.chdir("source")
-    lfs.mkdir("out")
-    lfs.chdir("out")
-    os.execute("cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=usr ..")
-    os.execute("cmake --build . --config Release --target install")
-end
-
+self.build = tools.build_cmake()
 function self.pack()
-    os.execute("cp -ra source/out/usr filesystem")
+    tools.pack_default()()
     os.execute("mv filesystem/usr/lib64 filesystem/usr/lib")
 end
 
