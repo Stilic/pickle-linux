b4834700ba5815bf7e0fb6d33e3ab13add28c79e
Stilic <lateman-jpeg@outlook.fr>
2025-09-25 22:58:33 +0200

    python-installer: rework

--- user/python-installer/init.lua
+++ user/python-installer/init.lua
@@ -1,25 +1,22 @@
 local lfs = require "lfs"
-local tools = require "tools"
 local python = pkg "user.python"
-local flit_core = pkg "user.python-flit-core"
 
 local self = {}
 
 self.version = "0.7.0"
 self.dependencies = { pkg "user.python" }
-self.dev_dependencies = { pkg "user.python-flit-core" }
 self.sources = {
-    { "source", "https://github.com/pypa/installer/archive/refs/tags/" .. self.version .. ".tar.gz" },
-    { "flit",   flit_core.sources[1][2] }
+    { "source", "https://files.pythonhosted.org/packages/py3/i/installer/installer-" .. self.version .. "-py3-none-any.whl" }
 }
 
-self.build = tools.build_flit()
 function self.pack()
-    lfs.chdir("flit/flit_core")
+    local install_dir = "filesystem/usr/lib/python" .. python.short_version .. "/site-packages"
 
-    os.execute("python bootstrap_install.py --installdir ../../filesystem/usr/lib/python" ..
-        python.short_version .. "/site-packages ../../source/dist/*.whl")
-    os.execute("install -Dm644 ../../source/LICENSE -t ../filesystem/usr/share/licenses/" .. self.name)
+    os.execute("mkdir -p " .. install_dir)
+    lfs.chdir("source")
+
+    os.execute("cp -a installer* ../" .. install_dir)
+    os.execute("python3 -m compileall ../" .. install_dir)
 end
 
 return self
