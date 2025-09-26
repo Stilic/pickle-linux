e19bf0d96ce45507fe91f1836c14b7f2b6f0e2bf
Stilic <lateman-jpeg@outlook.fr>
2025-09-26 21:35:48 +0200

    cmake: fix include path

--- user/cmake/init.lua
+++ user/cmake/init.lua
@@ -11,7 +11,7 @@ self.sources = {
 
 function self.build()
     lfs.chdir("source")
-    local bootstrap_cmd = './bootstrap CFLAGS="' ..
+    local bootstrap_cmd = './bootstrap CPATH=/include:/usr/include CFLAGS="' ..
         tools.DEFAULT_CFLAGS ..
         '" --prefix=/usr --mandir=/share/man --datadir=/share/' .. self.name .. ' --docdir=/share/doc/' .. self.name
     if system.build_cores ~= 1 then
