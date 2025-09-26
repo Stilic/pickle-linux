0a7e9e9cb335644c86fec4750058601db7849664
Stilic <lateman-jpeg@outlook.fr>
2025-09-26 22:57:47 +0200

    oops

--- user/slibtool/init.lua
+++ user/slibtool/init.lua
@@ -4,7 +4,7 @@ local self = {}
 
 self.version = "0.7.3"
 self.sources = {
-    { "source", "https://dev.midipix.org/cross/slibtool/archive/v" .. self.archive .. "/slibtool-v" .. self.version .. ".tar.gz" }
+    { "source", "https://dev.midipix.org/cross/slibtool/archive/v" .. self.version .. "/slibtool-v" .. self.version .. ".tar.gz" }
 }
 
 self.build = tools.build_gnu_configure()
