a83d5650b001889b6f272d187e11cd823bf063d2
Stilic <lateman-jpeg@outlook.fr>
2025-09-27 08:20:11 +0200

    musl-obstack: switch to Stilic's fork

--- main/musl-obstack/init.lua
+++ main/musl-obstack/init.lua
@@ -2,12 +2,12 @@ local tools = require "tools"
 
 local self = {}
 
-self.version = "1.2.3"
+self.version = "1.2.7-mk1"
 self.sources = {
-    { "source", "https://github.com/void-linux/musl-obstack/archive/refs/tags/v" .. self.version .. ".tar.gz" }
+    { "source", "https://github.com/Stilic/musl-obstack/archive/refs/tags/v" .. self.version .. ".tar.gz" }
 }
 
-self.build = tools.build_autotools("")
+self.build = tools.build_meson("")
 self.pack = tools.pack_default()
 
 return self
