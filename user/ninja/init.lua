3550ea0e903993b885f61091492555f94963c1b5
Stilic <lateman-jpeg@outlook.fr>
2025-09-27 19:01:24 +0200

    ninja: remove cmake dep

--- user/ninja/init.lua
+++ user/ninja/init.lua
@@ -1,15 +1,18 @@
+local lfs = require "lfs"
 local tools = require "tools"
 
 local self = {}
 
 self.version = "1.13.1"
 self.dependencies = { pkg "user.python" }
-self.dev_dependencies = { pkg "user.cmake" }
 self.sources = {
     { "source", "https://github.com/ninja-build/ninja/archive/refs/tags/v" .. self.version .. ".tar.gz" }
 }
 
-self.build = tools.build_cmake()
+function self.build()
+    lfs.chdir("source")
+    os.execute("python configure.py --bootstrap")
+end
 self.pack = tools.pack_default()
 
 return self
