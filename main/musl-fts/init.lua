d3509578f3288348d539347f53d665e63c8d6172
Stilic <lateman-jpeg@outlook.fr>
2025-09-27 08:07:40 +0200

    musl-fts: rework again

--- main/musl-fts/init.lua
+++ main/musl-fts/init.lua
@@ -2,12 +2,12 @@ local tools = require "tools"
 
 local self = {}
 
-self.version = "1.2.7"
+self.version = "1.2.7-mk2"
 self.sources = {
-    { "source", "https://github.com/void-linux/musl-fts/archive/refs/tags/v" .. self.version .. ".tar.gz" }
+    { "source", "https://github.com/chimera-linux/musl-fts/archive/refs/tags/v" .. self.version .. ".tar.gz" }
 }
 
-self.build = tools.build_autotools("")
+self.build = tools.build_gnu_configure("")
 self.pack = tools.pack_default()
 
 return self
