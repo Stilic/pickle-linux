264654c02287462a489d7c94d3da2bae22daeaab
Stilic <lateman-jpeg@outlook.fr>
2025-09-27 08:32:08 +0200

    libpsl: add missing dep

--- user/libpsl/init.lua
+++ user/libpsl/init.lua
@@ -3,7 +3,7 @@ local tools = require "tools"
 local self = {}
 
 self.version = "0.21.5"
-self.dev_dependencies = { pkg "user.libidn2", pkg "user.libunistring", pkg "user.python" }
+self.dev_dependencies = { pkg "user.libiconv", pkg "user.libidn2", pkg "user.libunistring", pkg "user.python" }
 self.sources = {
     { "source", "https://github.com/rockdaboot/libpsl/releases/download/" .. self.version .. "/libpsl-" .. self.version .. ".tar.gz" }
 }
