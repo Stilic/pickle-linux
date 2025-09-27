4e017ecb72097ff1959642289ceca1fe4f9f5d86
Stilic <lateman-jpeg@outlook.fr>
2025-09-27 18:54:46 +0200

    b

--- user/cmake/init.lua
+++ user/cmake/init.lua
@@ -12,14 +12,14 @@ self.sources = {
 
 function self.build()
     lfs.chdir("source")
-    local bootstrap_cmd = './bootstrap CFLAGS="' ..
+    local bootstrap_cmd = './bootstrap CPLUS_INCLUDE_PATH=/usr/include/c++/' .. gcc.version .. ' CFLAGS="' ..
         tools.DEFAULT_CFLAGS ..
         '" --prefix=/usr --mandir=/share/man --datadir=/share/' .. self.name .. ' --docdir=/share/doc/' .. self.name
     if system.build_cores ~= 1 then
         bootstrap_cmd = bootstrap_cmd .. " --parallel=" .. system.build_cores
     end
     os.execute(bootstrap_cmd)
-    os.execute("CPLUS_INCLUDE_PATH=/usr/include/c++/" .. gcc.version .. " make" .. system.get_make_jobs())
+    os.execute("make" .. system.get_make_jobs())
     lfs.mkdir("_install")
     os.execute('make install DESTDIR="' .. lfs.currentdir() .. '/_install"')
 end
