50598c96fd7440b495f660ef4d8b8f507f49aac2
Stilic <lateman-jpeg@outlook.fr>
2025-09-27 22:19:29 +0200

    revert to libtool

--- user/libtool/init.lua
+++ user/libtool/init.lua
@@ -2,9 +2,9 @@ local tools = require "tools"
 
 local self = {}
 
-self.version = "0.7.3"
+self.version = "2.5.4"
 self.sources = {
-    { "source", "https://git.foss21.org/slibtool/snapshot/slibtool-" .. self.version .. ".tar.bz2" }
+    { "source", "https://ftpmirror.gnu.org/libtool/libtool-" .. self.version .. ".tar.gz" }
 }
 
 self.build = tools.build_gnu_configure()
