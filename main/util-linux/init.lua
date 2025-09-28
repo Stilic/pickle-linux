bbcae40d6cc6e0dbf76acb28f17e44843cee81a6
Stilic <lateman-jpeg@outlook.fr>
2025-09-28 09:22:43 +0200

    util-linux: build libmount

--- main/util-linux/init.lua
+++ main/util-linux/init.lua
@@ -7,7 +7,8 @@ self.sources = {
     { "source", "https://www.kernel.org/pub/linux/utils/util-linux/v" .. self.version .. "/util-linux-" .. self.version .. ".tar.xz" }
 }
 
-self.build = tools.build_meson("/", "-Dbuild-newgrp=disabled -Dbuild-more=disabled -Dbuild-kill=disabled -Dbuild-nologin=disabled -Dbuild-liblastlog2=disabled -Dbuild-pam-lastlog2=disabled")
+self.build = tools.build_meson("/",
+    "-Dbuild-libmount=enabled -Dbuild-newgrp=disabled -Dbuild-more=disabled -Dbuild-kill=disabled -Dbuild-nologin=disabled -Dbuild-liblastlog2=disabled -Dbuild-pam-lastlog2=disabled")
 self.pack = tools.pack_default()
 
 return self
