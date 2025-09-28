dbc23df5bbeca40d851904a627d5eced59471239
Stilic <lateman-jpeg@outlook.fr>
2025-09-28 19:12:17 +0200

    add lullaby

--- main/lzo/init.lua
+++ main/lzo/init.lua
@@ -1,13 +1,22 @@
+local lfs = require "lfs"
+local system = require "system"
 local tools = require "tools"
 
 local self = {}
 
 self.version = "2.10"
+-- TODO: switch to official release
 self.sources = {
-    { "source", "https://www.oberhumer.com/opensource/lzo/download/lzo-" .. self.version .. ".tar.gz" }
+    { "source", "https://github.com/Stilic/lullaby/archive/refs/tags/v0.0.2.tar.gz" }
 }
 
-self.build = tools.build_gnu_configure("", "--enable-shared")
-self.pack = tools.pack_default()
+function self.build()
+    lfs.chdir("source")
+    os.execute(tools.get_flags() .. " make CC=gcc" .. system.get_make_jobs())
+end
+function self.pack()
+    lfs.mkdir("filesystem/lib")
+    os.execute("cp -a lullaby.so filesystem/lib")
+end
 
 return self
