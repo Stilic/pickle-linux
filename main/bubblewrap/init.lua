312c7982befd72d2dd109a13e645038ea5bd382b
Stilic <lateman-jpeg@outlook.fr>
2025-09-28 23:38:35 +0200

    add bubblewrap

---
+++ main/bubblewrap/init.lua
@@ -0,0 +1,14 @@
+local tools = require "tools"
+
+local self = {}
+
+self.version = "0.11.0"
+self.dev_dependencies = { pkg "user.meson", pkg "user.pkgconf" }
+self.sources = {
+    { "source", "https://github.com/containers/bubblewrap/releases/download/v" .. self.version .. "/bubblewrap-" .. self.version .. ".tar.xz" }
+}
+
+self.build = tools.build_meson("/")
+self.pack = tools.pack_default()
+
+return self
