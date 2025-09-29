9ecf8b3b6e9c54a896277f107465045a36219eb8
Stilic <lateman-jpeg@outlook.fr>
2025-09-29 07:47:39 +0200

    python: fix

--- user/python/init.lua
+++ user/python/init.lua
@@ -10,7 +10,7 @@ self.sources = {
     { "source", "https://www.python.org/ftp/python/" .. self.version .. "/Python-" .. self.version .. ".tar.xz" }
 }
 
-self.build = tools.build_gnu_configure(nil, "--enable-shared --with-system-expat --without-ensurepip")
+self.build = tools.build_gnu_configure("--enable-shared --with-system-expat --without-ensurepip")
 function self.pack()
     tools.pack_default()()
 
