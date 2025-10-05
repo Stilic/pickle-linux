47864674c9564dabffa607a5b032fdea2ed1103a
Stilic <lateman-jpeg@outlook.fr>
2025-10-05 23:55:32 +0200

    gcc: fix prefix it cannot find libgcc itself

--- user/gcc/init.lua
+++ user/gcc/init.lua
@@ -21,14 +21,14 @@ function self.build()
     lfs.chdir("build")
 
     os.execute(tools.get_flags() ..
-        " ../configure --prefix= --disable-multilib --disable-nls --with-system-zlib --with-native-system-header-dir=/include --enable-default-pie --enable-default-ssp --enable-host-pie --enable-languages=c,c++")
+        " ../configure --prefix=/usr --disable-multilib --disable-nls --with-system-zlib --with-native-system-header-dir=/include --enable-default-pie --enable-default-ssp --enable-host-pie --enable-languages=c,c++")
     os.execute("CPATH=/usr/include make" .. system.get_make_jobs())
 
     os.execute('make install-strip DESTDIR="' .. install_dir .. '"')
 end
 
 function self.pack()
-    tools.pack_default()()
+    tools.pack_default("source/_install/usr")()
     os.execute("rm -r filesystem/lib filesystem/lib64")
     lfs.link("gcc", "filesystem/bin/cc", true)
 end
@@ -36,8 +36,8 @@ end
 self.variants = {
     libs = {
         pack = function()
-            os.execute("cp -ra source/_install/lib filesystem-libs")
-            os.execute("cp -ra source/_install/lib64/* filesystem-libs/lib")
+            os.execute("cp -ra source/_install/usr/lib filesystem-libs")
+            os.execute("cp -ra source/_install/usr/lib64/* filesystem-libs/lib")
         end
     }
 }
