73b4eec430f92a20ec3caa98676457832ed22771
Stilic <lateman-jpeg@outlook.fr>
2025-10-05 23:52:19 +0200

    perl: fix prefix it has to be hardcoded to `/usr` or stuff will break

--- user/perl/init.lua
+++ user/perl/init.lua
@@ -12,12 +12,12 @@ self.sources = {
 function self.build()
     lfs.chdir("source")
     os.execute(
-        './Configure -des -Dprefix=/ -Dman1ext=1p -Dman3ext=3p -Dman1dir=/share/man/man1 -Dman3dir=/share/man/man3 -Dccflags="' ..
+        './Configure -des -Dprefix=/usr -Dman1ext=1p -Dman3ext=3p -Dman1dir=/share/man/man1 -Dman3dir=/share/man/man3 -Dccflags="' ..
         tools.DEFAULT_CFLAGS .. ' -Wno-implicit-function-declaration"')
     os.execute("make" .. system.get_make_jobs())
     os.execute("make install DESTDIR='" .. lfs.currentdir() .. "/_install'")
 end
 
-self.pack = tools.pack_default()
+self.pack = tools.pack_default("source/_install/usr")
 
 return self
