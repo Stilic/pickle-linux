4bc913a8ed2d361f7c16c9bca093b281d23e505d
Stilic <lateman-jpeg@outlook.fr>
2025-10-06 23:57:15 +0200

    duh

--- user/bison/init.lua
+++ user/bison/init.lua
@@ -8,7 +8,7 @@ self.sources = {
     { "source", config.gnu_site .. "/bison/bison-" .. self.version .. ".tar.xz" }
 }
 
-self.build = tools.build_gnu_configure()
-self.pack = tools.pack_default()
+self.build = tools.build_gnu_configure("--prefix=/usr")
+self.pack = tools.pack_default("source/_install/usr")
 
 return self
