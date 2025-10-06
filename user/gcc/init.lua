bc293e40630e80505e8851f3b38a5b2bb9d0e23c
Stilic <lateman-jpeg@outlook.fr>
2025-10-06 22:32:02 +0200

    ddudueh

--- user/gcc/init.lua
+++ user/gcc/init.lua
@@ -21,7 +21,7 @@ function self.build()
     lfs.chdir("build")
 
     os.execute(tools.get_flags() ..
-        " ../configure --prefix=/usr --libdir=/lib --with-gxx-include-dir=/include --with-native-system-header-dir=/include --disable-multilib --disable-nls --with-system-zlib --enable-default-pie --enable-default-ssp --enable-host-pie --enable-languages=c,c++")
+        " ../configure --prefix=/usr --libdir=/lib --with-native-system-header-dir=/include --disable-multilib --disable-nls --with-system-zlib --enable-default-pie --enable-default-ssp --enable-host-pie --enable-languages=c,c++")
     os.execute("CPATH=/usr/include make" .. system.get_make_jobs())
 
     os.execute('make install-strip DESTDIR="' .. install_dir .. '"')
@@ -29,13 +29,14 @@ end
 
 function self.pack()
     tools.pack_default("source/_install/usr")()
+    os.execute("rm -r filesystem/include")
     lfs.link("gcc", "filesystem/bin/cc", true)
 end
 
 self.variants = {
     libs = {
         pack = function()
-            os.execute("cp -ra source/_install/lib source/_install/include filesystem-libs")
+            os.execute("cp -ra source/_install/lib source/_install/usr/include filesystem-libs")
             os.execute("cp -ra source/_install/lib64/* filesystem-libs/lib")
         end
     }
