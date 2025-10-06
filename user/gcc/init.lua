3057a12fb78d3eab1a3c0c5f77275fbd59a8ab24
Stilic <lateman-jpeg@outlook.fr>
2025-10-06 09:00:44 +0200

    ??

--- user/gcc/init.lua
+++ user/gcc/init.lua
@@ -21,7 +21,7 @@ function self.build()
     lfs.chdir("build")
 
     os.execute(tools.get_flags() ..
-        " ../configure --prefix= --disable-multilib --disable-nls --with-system-zlib --enable-default-pie --enable-default-ssp --enable-host-pie --enable-languages=c,c++")
+        " ../configure --prefix= --disable-multilib --disable-nls --with-system-zlib --with-native-system-header-dir=/include --enable-default-pie --enable-default-ssp --enable-host-pie --enable-languages=c,c++")
     os.execute("CPATH=/usr/include make" .. system.get_make_jobs())
 
     os.execute('make install-strip DESTDIR="' .. install_dir .. '"')
