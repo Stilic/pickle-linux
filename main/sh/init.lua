c09d9dc0847b96df0f87a3155c495aa1eb3127fe
Stilic <lateman-jpeg@outlook.fr>
2025-10-12 17:34:58 +0200

    replace bash by oksh

---
+++ main/sh/init.lua
@@ -0,0 +1,22 @@
+local lfs = require "lfs"
+
+local self = {}
+
+self.version = "7.7"
+self.sources = {
+    { "source", "https://github.com/ibara/oksh/releases/download/oksh-" .. self.version .. "/oksh-" .. self.version .. ".tar.gz" }
+}
+
+function self.build()
+    lfs.chdir("source")
+
+    os.execute("cp ../../pconfig.h .")
+    os.execute("gcc -o oksh -D_GNU_SOURCE -DEMACS -DVI -DSMALL *.c")
+end
+
+function self.pack()
+    lfs.mkdir("filesystem/bin")
+    os.execute("cp source/oksh filesystem/bin/sh")
+end
+
+return self
