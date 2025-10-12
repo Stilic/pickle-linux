9aaef4bbbb225ba3060d82bb745d3adb318ecf27
Stilic <lateman-jpeg@outlook.fr>
2025-10-12 09:35:42 +0200

    add missing meson dep

--- main/musl-obstack/init.lua
+++ main/musl-obstack/init.lua
@@ -3,6 +3,7 @@ local tools = require "tools"
 local self = {}
 
 self.version = "1.2.7-mk1"
+self.dev_dependencies = { pkg "user.meson" }
 self.sources = {
     { "source", "https://github.com/Stilic/musl-obstack/archive/refs/tags/v" .. self.version .. ".tar.gz" }
 }
