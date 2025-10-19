1e00240a87a016fd836be6412ddc080fda57aa45
Stilic <lateman-jpeg@outlook.fr>
2025-10-19 22:02:00 +0200

    duh

--- main/util-linux/init.lua
+++ main/util-linux/init.lua
@@ -3,13 +3,17 @@ local tools = require "tools"
 local self = {}
 
 self.version = "2.41"
-self.dev_dependencies = { pkg "user.meson", pkg "user.bash" }
+self.dev_dependencies = { pkg "user.meson" }
 self.sources = {
     { "source", "https://www.kernel.org/pub/linux/utils/util-linux/v" .. self.version .. "/util-linux-" .. self.version .. ".tar.xz" }
 }
 
-self.build = tools.build_meson(
-    "-Dbuild-newgrp=disabled -Dbuild-more=disabled -Dbuild-kill=disabled -Dbuild-nologin=disabled -Dbuild-liblastlog2=disabled -Dbuild-pam-lastlog2=disabled")
+function self.build()
+    os.execute("find . -type f -exec sed -i 's|^#!/bin/bash|#!/bin/sh|' {} +")
+    tools.build_meson(
+        "-Dbuild-newgrp=disabled -Dbuild-more=disabled -Dbuild-kill=disabled -Dbuild-nologin=disabled -Dbuild-liblastlog2=disabled -Dbuild-pam-lastlog2=disabled")()
+end
+
 self.pack = tools.pack_default()
 
 return self
