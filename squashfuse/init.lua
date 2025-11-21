f4b2707b58c4bc523484d82073a3563135b63f13
Stilic <lateman-jpeg@outlook.fr>
2025-11-21 19:19:18 +0100

    add squashfuse

---
+++ squashfuse/init.lua
@@ -0,0 +1,12 @@
+local tools = require "tools"
+
+version = "0.6.1"
+-- TODO: install lzo/attr as a dep
+-- dependencies = { pkg "lzo", pkg "attr" }
+sources = {
+    { "source", "https://github.com/vasi/squashfuse/releases/download/" .. version .. "/squashfuse-" .. version .. ".tar.gz" }
+}
+
+build = tools.build_gnu_configure()
+
+pack = tools.pack_default()
