1d5d9b0f3315fb6003ad26095c155ea8534739dd
Stilic <lateman-jpeg@outlook.fr>
2025-11-23 09:43:06 +0100

    cmake: add ninja dep

--- llvm/init.lua
+++ llvm/init.lua
@@ -1,7 +1,7 @@
 local tools = require "tools"
 
 version = "21.1.6"
-dev_dependencies = { pkg "cmake" }
+dev_dependencies = { pkg "cmake", pkg "python" }
 sources = {
     { "source", "https://github.com/llvm/llvm-project/releases/download/llvmorg-" .. version .. "/llvm-project-" .. version .. ".src.tar.xz" }
 }
