82e7ead3bd5f6b0db87065134204343508c2bd04
Stilic <lateman-jpeg@outlook.fr>
2025-11-29 22:32:27 +0100

    llvm: fix???? final?

--- llvm/init.lua
+++ llvm/init.lua
@@ -36,20 +36,17 @@ local function gen_build(external_llvm, projects, runtimes)
     end
 
     return function()
-        local external_command, targets = ""
+        local external_command = ""
         if external_llvm then
             local build_dir = lfs.currentdir()
-            external_command =
-                "-DCLANG_ENABLE_BOOTSTRAP=OFF -DLLVM_DISTRIBUTION_COMPONENTS= -DLLVM_RUNTIME_DISTRIBUTION_COMPONENTS=" ..
-                runtimes:gsub("compiler%-rt;", "") .. " -DLLVM_EXTERNAL_LIT=" ..
+            external_command = "-DLLVM_EXTERNAL_LIT=" ..
                 build_dir .. "/source/build-llvm/utils/lit -DLLVM_ROOT=" .. build_dir .. "/filesystem "
-            targets = { "clang-resource-headers", "install-distribution" }
         end
         tools.build_cmake(external_command ..
             "-DLLVM_PARALLEL_LINK_JOBS=2 -DLLVM_INSTALL_TOOLCHAIN_ONLY=ON -DLLVM_INCLUDE_TESTS=OFF -DLLVM_TARGETS_TO_BUILD=" ..
             arch .. projects_command .. runtimes_command ..
             "-DCOMPILER_RT_USE_BUILTINS_LIBRARY=ON -DCOMPILER_RT_DEFAULT_TARGET_ONLY=ON -DCOMPILER_RT_BUILD_GWP_ASAN=OFF -DLIBCXX_CXX_ABI=libcxxabi -DLIBCXX_USE_COMPILER_RT=ON -DLIBCXX_ENABLE_STATIC_ABI_LIBRARY=OFF -DLIBCXX_HAS_MUSL_LIBC=ON -DLIBCXX_HARDENING_MODE=fast -DLIBCXXABI_USE_LLVM_UNWINDER=ON -DLIBCXXABI_ENABLE_STATIC_UNWINDER=OFF -DLIBCXXABI_USE_COMPILER_RT=ON -DLLVM_INSTALL_UTILS=ON -DLLVM_BUILD_LLVM_DYLIB=ON -DLLVM_LINK_LLVM_DYLIB=ON -DLLVM_ENABLE_RTTI=ON -DLLVM_ENABLE_PER_TARGET_RUNTIME_DIR=ON -DCLANG_DEFAULT_RTLIB=compiler-rt -DCLANG_DEFAULT_UNWINDLIB=libunwind -DCLANG_DEFAULT_CXX_STDLIB=libc++ -DLLVM_ENABLE_LIBXML2=OFF -DLLVM_ENABLE_LIBCXX=ON -DLIBUNWIND_ENABLE_ASSERTIONS=OFF -DLIBUNWIND_USE_COMPILER_RT=ON",
-            nil, "llvm", targets)()
+            nil, "llvm")()
     end
 end
 
@@ -59,6 +56,6 @@ pack = tools.pack_default()
 
 variants = {
     libs = {
-        build = gen_build(true, "clang", "compiler-rt")
+        build = gen_build(true, nil, "compiler-rt")
     }
 }
