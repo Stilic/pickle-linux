6fadd05a746a07306df7a5be2fe0e55eb65bc257
Stilic <lateman-jpeg@outlook.fr>
2025-12-02 23:16:27 +0100

    llvm: merge all part-specific flags

--- llvm/init.lua
+++ llvm/init.lua
@@ -27,58 +27,54 @@ if buildmode then
     triplet = system.architecture .. "-pc-linux-musl"
 end
 local function gen_build(part, projects, runtimes)
-    local additional_command = ""
+    local options = ""
 
     if projects then
-        additional_command = "-DLLVM_ENABLE_PROJECTS="
+        options = "-DLLVM_ENABLE_PROJECTS="
 
         local l = #projects
         for i, project in ipairs(projects) do
-            additional_command = additional_command .. project
+            options = options .. project
             if i ~= l then
-                additional_command = additional_command .. ";"
+                options = options .. ";"
             end
         end
 
-        additional_command = additional_command .. " "
+        options = options .. " "
     end
 
     if runtimes then
-        additional_command = additional_command .. "-DLLVM_ENABLE_RUNTIMES="
+        options = options .. "-DLLVM_ENABLE_RUNTIMES="
 
         local l = #runtimes
         for i, runtime in ipairs(runtimes) do
-            additional_command = additional_command .. runtime
+            options = options .. runtime
             if i ~= l then
-                additional_command = additional_command .. ";"
+                options = options .. ";"
             end
         end
 
-        additional_command = additional_command .. " "
+        options = options .. " "
     end
 
     return function()
         if part ~= "llvm" then
             local build_dir = lfs.currentdir()
             local bin_dir = build_dir .. "/filesystem"
-            additional_command = additional_command .. "-DLLVM_EXTERNAL_LIT=" ..
+            options = options .. "-DLLVM_EXTERNAL_LIT=" ..
                 build_dir .. "/source/build-llvm/utils/lit -DLLVM_ROOT=" .. bin_dir .. " "
             if not hostfs then
                 bin_dir = bin_dir .. "/bin/"
-                additional_command = additional_command ..
+                options = options ..
                     "-DCMAKE_C_COMPILER=" .. bin_dir .. "clang -DCMAKE_CXX_COMPILER=" .. bin_dir .. "clang++ "
             end
         end
 
-        tools.build_cmake(additional_command .. "-DLLVM_TARGET_ARCH=" ..
-            arch ..
-            " -DLLVM_HOST_TRIPLE=" ..
-            triplet ..
-            " -DLLVM_DEFAULT_TARGET_TRIPLE=" ..
-            triplet .. " " ..
-            (part == "llvm"
-                and ("-DENABLE_LINKER_BUILD_ID=ON -DLLVM_INSTALL_BINUTILS_SYMLINKS=ON -DLLVM_INSTALL_UTILS=ON -DLLVM_BUILD_LLVM_DYLIB=ON -DLLVM_LINK_LLVM_DYLIB=ON -DLLVM_ENABLE_RTTI=ON -DLLVM_ENABLE_PER_TARGET_RUNTIME_DIR=ON -DLLVM_ENABLE_LIBXML2=OFF -DMLIR_INSTALL_AGGREGATE_OBJECTS=OFF" .. (hostfs and "" or " -DCLANG_DEFAULT_RTLIB=compiler-rt -DCLANG_DEFAULT_UNWINDLIB=libunwind -DCLANG_DEFAULT_CXX_STDLIB=libc++ -DLLVM_ENABLE_LIBCXX=ON"))
-                or "-DCOMPILER_RT_BUILD_GWP_ASAN=OFF -DLIBCXX_CXX_ABI=libcxxabi -DLIBCXX_ENABLE_STATIC_ABI_LIBRARY=OFF -DLIBCXX_HAS_MUSL_LIBC=ON -DLIBCXX_HARDENING_MODE=fast -DLIBCXXABI_ENABLE_STATIC_UNWINDER=OFF -DLIBUNWIND_ENABLE_ASSERTIONS=OFF -DLIBUNWIND_HAS_NODEFAULTLIBS_FLAG=OFF -DCOMPILER_RT_SCUDO_STANDALONE_BUILD_SHARED=OFF" .. (hostfs and "" or " -DCOMPILER_RT_DEFAULT_TARGET_ONLY=ON")),
+        tools.build_cmake(options .. "-DLLVM_TARGET_ARCH=" .. arch ..
+            " -DLLVM_HOST_TRIPLE=" .. triplet ..
+            " -DLLVM_DEFAULT_TARGET_TRIPLE=" .. triplet ..
+            " -DENABLE_LINKER_BUILD_ID=ON -DLLVM_INSTALL_BINUTILS_SYMLINKS=ON -DLLVM_INSTALL_UTILS=ON -DLLVM_BUILD_LLVM_DYLIB=ON -DLLVM_LINK_LLVM_DYLIB=ON -DLLVM_ENABLE_RTTI=ON -DLLVM_ENABLE_PER_TARGET_RUNTIME_DIR=ON -DLLVM_ENABLE_LIBXML2=OFF -DMLIR_INSTALL_AGGREGATE_OBJECTS=OFF -DCOMPILER_RT_BUILD_GWP_ASAN=OFF -DLIBCXX_CXX_ABI=libcxxabi -DLIBCXX_ENABLE_STATIC_ABI_LIBRARY=OFF -DLIBCXX_HAS_MUSL_LIBC=ON -DLIBCXX_HARDENING_MODE=fast -DLIBCXXABI_ENABLE_STATIC_UNWINDER=OFF -DLIBUNWIND_ENABLE_ASSERTIONS=OFF -DLIBUNWIND_HAS_NODEFAULTLIBS_FLAG=OFF -DCOMPILER_RT_SCUDO_STANDALONE_BUILD_SHARED=OFF" ..
+            (hostfs and "" or " -DCOMPILER_RT_DEFAULT_TARGET_ONLY=ON -DCLANG_DEFAULT_RTLIB=compiler-rt -DCLANG_DEFAULT_UNWINDLIB=libunwind -DCLANG_DEFAULT_CXX_STDLIB=libc++ -DLLVM_ENABLE_LIBCXX=ON"),
             nil, part)()
     end
 end
