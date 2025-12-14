00b109b7894c1906cf14b0eea9363874b361533a
Stilic <lateman-jpeg@outlook.fr>
2025-12-15 00:24:56 +0100

    gcc: build boostrap stuff

--- gcc/init.lua
+++ gcc/init.lua
@@ -3,7 +3,7 @@ local system = require "system"
 local tools = require "tools"
 local config = require "neld.config"
 
-version = "14.2.0"
+version = "13.1.0"
 sources = {
     { "source", config.gnu_site .. "/gcc/gcc-" .. version .. "/gcc-" .. version .. ".tar.xz" }
 }
@@ -13,29 +13,44 @@ function build()
 
     local install_dir = lfs.currentdir() .. "/_install"
 
-    lfs.mkdir("build")
-    lfs.chdir("build")
+    lfs.mkdir("build-libgcc")
+    lfs.chdir("build-libgcc")
 
     os.execute(tools.get_flags() ..
-        " ../configure --prefix=/usr --libdir=/lib --with-system-zlib --disable-multilib --disable-nls --enable-default-pie --enable-default-ssp --enable-host-pie --enable-languages=c,c++")
+        " ../configure --prefix=/usr --libdir=/lib --disable-bootstrap --disable-multilib --disable-nls --enable-default-pie --enable-default-ssp --enable-host-pie --enable-languages=c")
+    os.execute("CPATH=/usr/include make all-target-libgcc" .. system.get_make_jobs())
+
+    os.execute('make install-target-libgcc DESTDIR="' .. install_dir .. '"')
+
+    lfs.chdir("..")
+
+    lfs.mkdir("build-libstdc++")
+    lfs.chdir("build-libstdc++")
+
+    os.execute(tools.get_flags() ..
+        " ../libstdc++-v3/configure --prefix=/usr --libdir=/lib --disable-multilib --disable-nls --enable-host-pie")
     os.execute("CPATH=/usr/include make" .. system.get_make_jobs())
 
-    os.execute('make install-strip DESTDIR="' .. install_dir .. '"')
+    os.execute('make install DESTDIR="' .. install_dir .. '"')
 end
 
 function pack()
-    tools.pack_default("source/_install/usr")()
-    os.execute("rm -r filesystem/include")
+    tools.pack_default("source/_install")()
+
+    os.execute("mv filesystem/lib64/* filesystem/lib")
+    os.execute("mv filesystem/lib/gcc/*/*/* filesystem/lib")
+    os.execute("mv filesystem/lib/include filesystem")
+    os.execute("rm -r filesystem/lib64 filesystem/lib/gcc filesystem/usr")
 end
 
-variants = {
-    libs = {
-        pack = function()
-            os.execute("cp -ra source/_install/lib source/_install/usr/include filesystem-libs")
-            os.execute("cp -ra source/_install/lib64/* filesystem-libs/lib")
+-- variants = {
+--     libs = {
+--         pack = function()
+--             os.execute("cp -ra source/_install/lib source/_install/usr/include filesystem-libs")
+--             os.execute("cp -ra source/_install/lib64/* filesystem-libs/lib")
 
-            -- https://git.yoctoproject.org/poky/commit/?id=483143a38ec0ac7b12b9cdf3cd5ce79d8f20cb2f
-            os.execute("find filesystem-libs -type f -exec sed -i '' 's/#include_next/#include/g' {} +")
-        end
-    }
-}
+--             -- https://git.yoctoproject.org/poky/commit/?id=483143a38ec0ac7b12b9cdf3cd5ce79d8f20cb2f
+--             os.execute("find filesystem-libs -type f -exec sed -i '' 's/#include_next/#include/g' {} +")
+--         end
+--     }
+-- }
--- llvm/init.lua
+++ llvm/init.lua
@@ -68,40 +68,35 @@ local function gen_build(part, projects, runtimes)
             options = options .. "-DLLVM_EXTERNAL_LIT=" ..
                 build_dir .. "/source/build-llvm/utils/lit -DLLVM_ROOT=" .. bin_dir .. " "
             bin_dir = bin_dir .. "/bin/"
-            options = options ..
-                "-DCMAKE_C_COMPILER=" ..
-                (hostfs and "gcc" or (bin_dir .. "clang")) ..
-                " -DCMAKE_CXX_COMPILER=" .. (hostfs and "g++" or (bin_dir .. "clang++")) .. " "
+            -- options = options ..
+            --     "-DCMAKE_C_COMPILER=" ..
+            --     bin_dir .. "clang" ..
+            --     " -DCMAKE_CXX_COMPILER=" .. bin_dir .. "clang++" .. " "
         end
 
         local compiler_flags = "-I" .. build_dir .. "/filesystem-unwind/include"
         tools.build_cmake(
             options ..
-            (hostfs and ("-DLLVM_TARGETS_TO_BUILD=" .. arch .. " -DCOMPILER_RT_BUILD_SANITIZERS=OFF ") or "") ..
+            (hostfs and ("-DLLVM_TARGETS_TO_BUILD=" .. arch .. " -DLIBCXX_USE_COMPILER_RT=OFF -DLIBCXX_USE_STATIC_ABI_LIBRARY=OFF ") or "") ..
             "-DLLVM_TARGET_ARCH=" .. arch ..
             " -DLLVM_HOST_TRIPLE=" .. triplet ..
             " -DLLVM_DEFAULT_TARGET_TRIPLE=" .. triplet ..
-            " -DENABLE_LINKER_BUILD_ID=ON -DLLVM_INSTALL_BINUTILS_SYMLINKS=ON -DLLVM_INSTALL_UTILS=ON -DLLVM_BUILD_LLVM_DYLIB=ON -DLLVM_LINK_LLVM_DYLIB=ON -DLLVM_ENABLE_RTTI=ON -DLLVM_ENABLE_PER_TARGET_RUNTIME_DIR=ON -DLLVM_ENABLE_LIBXML2=OFF -DMLIR_INSTALL_AGGREGATE_OBJECTS=OFF -DCOMPILER_RT_BUILD_LIBFUZZER=OFF -DCOMPILER_RT_BUILD_GWP_ASAN=OFF -DLIBCXX_CXX_ABI=libcxxabi -DLIBCXX_ENABLE_STATIC_ABI_LIBRARY=OFF -DLIBCXX_HAS_MUSL_LIBC=ON -DLIBCXX_HARDENING_MODE=fast -DLIBCXXABI_ENABLE_STATIC_UNWINDER=OFF -DLIBUNWIND_ENABLE_ASSERTIONS=OFF -DLIBUNWIND_HAS_NODEFAULTLIBS_FLAG=OFF -DCOMPILER_RT_SCUDO_STANDALONE_BUILD_SHARED=OFF -DCLANG_DEFAULT_RTLIB=compiler-rt -DCLANG_DEFAULT_CXX_STDLIB=libc++ -DLLVM_ENABLE_LIBCXX=ON",
+            " -DENABLE_LINKER_BUILD_ID=ON -DLLVM_INSTALL_BINUTILS_SYMLINKS=ON -DLLVM_INSTALL_UTILS=ON -DLLVM_BUILD_LLVM_DYLIB=ON -DLLVM_LINK_LLVM_DYLIB=ON -DLLVM_ENABLE_RTTI=ON -DLLVM_ENABLE_PER_TARGET_RUNTIME_DIR=ON -DLLVM_ENABLE_LIBXML2=OFF -DMLIR_INSTALL_AGGREGATE_OBJECTS=OFF -DCOMPILER_RT_BUILD_GWP_ASAN=OFF -DLIBCXX_CXX_ABI=libcxxabi -DLIBCXX_ENABLE_STATIC_ABI_LIBRARY=OFF -DLIBCXX_HAS_MUSL_LIBC=ON -DLIBCXX_HARDENING_MODE=fast -DLIBCXXABI_ENABLE_STATIC_UNWINDER=OFF -DLIBUNWIND_ENABLE_ASSERTIONS=OFF -DCOMPILER_RT_SCUDO_STANDALONE_BUILD_SHARED=OFF -DCLANG_DEFAULT_RTLIB=compiler-rt -DCLANG_DEFAULT_CXX_STDLIB=libc++ -DLLVM_ENABLE_LIBCXX=ON -DLLVM_INCLUDE_TESTS=OFF",
             nil, part, nil, compiler_flags, compiler_flags, "-L" .. build_dir .. "/filesystem-unwind/lib")()
     end
 end
 
-local runtimes = { "compiler-rt", "libcxx", "libcxxabi" }
 if hostfs then
     variants = {
         unwind = {
             build = gen_build("runtimes", nil, { "libunwind" }),
             pack = tools.pack_default(nil, "unwind")
-        },
-        libs = {
-            build = gen_build("runtimes", nil, runtimes),
-            pack = tools.pack_default(nil, "libs")
         }
     }
 else
     variants = {
         libs = {
-            build = gen_build("runtimes", nil, runtimes),
+            build = gen_build("runtimes", nil, { "compiler-rt", "libcxx", "libcxxabi", "libunwind" }),
             pack = tools.pack_default(nil, "libs")
         }
     }
