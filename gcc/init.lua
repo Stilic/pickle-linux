2f6600cf58a1d0664ec00ba634652660b049397b
Stilic <lateman-jpeg@outlook.fr>
2025-12-20 09:37:29 +0100

    Update init.lua

--- gcc/init.lua
+++ gcc/init.lua
@@ -17,13 +17,18 @@ function build()
     lfs.mkdir("build")
     lfs.chdir("build")
 
-    os.execute(tools.get_flags() ..
-        " ../configure --prefix=/usr --libdir=/lib --disable-multilib --disable-nls --enable-default-pie --enable-default-ssp --enable-host-pie --enable-languages=c,c++ --host=" ..
-        system.target .. " --build=" .. system.target .. (stage == 1 and " --disable-bootstrap" or ""))
+    local flags = " --prefix=/usr --libdir=/lib --disable-multilib --disable-nls --host=" ..
+        system.target .. " --build=" .. system.target
 
-    os.execute((stage == 1 and ("CPLUS_INCLUDE_PATH=/usr/include/c++:/usr/include/c++/" ..
-        system.target .. " ") or "") .. "make" .. system.get_make_jobs())
+    os.execute(tools.get_flags() .. " ../libstdc++-v3/configure" .. flags)
+    os.execute("make" .. system.get_make_jobs())
     os.execute('make install DESTDIR="' .. install_dir .. '"')
+
+    os.execute(tools.get_flags() ..
+        " ../configure --enable-default-pie --enable-default-ssp --enable-host-pie --enable-languages=c,c++" ..
+        flags .. (stage == 1 and " --disable-bootstrap" or ""))
+    os.execute("make all-gcc " .. system.get_make_jobs())
+    os.execute('make install-gcc DESTDIR="' .. install_dir .. '"')
 end
 
 function pack()
