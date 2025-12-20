9de77d45ea6acd9733051091c90e293f4a9db17f
Stilic <lateman-jpeg@outlook.fr>
2025-12-20 20:24:33 +0100

    fix stage2

--- gcc-libs/init.lua
+++ gcc-libs/init.lua
@@ -17,7 +17,8 @@ function build()
     lfs.mkdir("build")
     lfs.chdir("build")
 
-    local flags = " --prefix= --disable-multilib --disable-nls --host=" .. system.target .. " --build=" .. system.target
+    local flags = " --prefix= --with-gxx-include-dir=/include/c++ --disable-multilib --disable-nls --host=" ..
+        system.target .. " --build=" .. system.target
 
     if stage == 1 then
         os.execute(tools.get_flags() .. " ../libstdc++-v3/configure" .. flags)
@@ -31,10 +32,12 @@ function build()
         flags)
 
     os.execute("make all-target-libgcc" .. system.get_make_jobs())
+    os.execute("make all-target-libatomic" .. system.get_make_jobs())
     if stage ~= 1 then
         os.execute("make all-target-libstdc++-v3" .. system.get_make_jobs())
     end
     os.execute('make install-target-libgcc DESTDIR="' .. install_dir .. '"')
+    os.execute('make install-target-libatomic DESTDIR="' .. install_dir .. '"')
     if stage ~= 1 then
         os.execute('make install-target-libstdc++-v3 DESTDIR="' .. install_dir .. '"')
     end
@@ -45,6 +48,14 @@ function pack()
 
     os.execute("mv filesystem/lib64/* filesystem/lib")
     os.execute("rm -r filesystem/lib64")
+
+    if stage ~= 1 then
+        os.execute("mv filesystem/lib/gcc/*/*/include/* filesystem/include")
+        os.execute("mv filesystem/lib/gcc/*/*/* filesystem/lib")
+        os.execute("rm -r filesystem/lib/gcc")
+    end
+
+    os.execute("rm -r filesystem/share")
 
     os.execute("find filesystem -type f -exec sed -i 's/#include_next/#include/g' {} +")
 end
