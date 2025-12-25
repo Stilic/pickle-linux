9c1f71bfeab93d17a38c55a4200b999bd245a8a6
Stilic <lateman-jpeg@outlook.fr>
2025-12-25 17:24:20 +0100

    dhcpcd: fix mandir/datadir

--- dhcpcd/init.lua
+++ dhcpcd/init.lua
@@ -5,6 +5,6 @@ sources = {
     { "source", "https://github.com/NetworkConfiguration/dhcpcd/releases/download/v" .. version .. "/dhcpcd-" .. version .. ".tar.xz" }
 }
 
-build = tools.build_gnu_configure()
+build = tools.build_gnu_configure("--mandir=/share/man --datadir=/share")
 
 pack = tools.pack_default()
